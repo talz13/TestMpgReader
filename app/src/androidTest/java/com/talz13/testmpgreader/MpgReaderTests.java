787a79c5aca17ca873dc2c8b271d28a9bfc5d9c4
Jeffrey Byrom <talz13@gmail.com>
2015-06-02 21:59:09 -0400

    Committing a few more working tests...

--- app/src/androidTest/java/com/talz13/testmpgreader/MpgReaderTests.java
+++ app/src/androidTest/java/com/talz13/testmpgreader/MpgReaderTests.java
@@ -8,34 +8,59 @@ import android.test.AndroidTestCase;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-//public class ApplicationTest extends ApplicationTestCase<Application> {
-//    public ApplicationTest() {
-//        super(Application.class);
-//    }
 
 public class MpgReaderTests extends AndroidTestCase {
 
     public void testOpeningFile() {
         MpgFile mpgFile = new MpgFile(getContext());
+        String fileName = mpgFile.getContext().getString(R.string.TestFile);
 
-        boolean openedFile = mpgFile.OpenFile();
+        boolean openedFile = mpgFile.OpenFile(fileName);
 
         assertEquals(true, openedFile);
     }
 
+    public void testClosingFile() {
+        MpgFile mpgFile = new MpgFile(getContext());
+        String fileName = mpgFile.getContext().getString(R.string.TestFile);
+        boolean openedFile = mpgFile.OpenFile(fileName);
+
+        boolean closedFile = mpgFile.CloseFile();
+
+        assertEquals(true, closedFile);
+    }
+
     public void testReadingHeaderLine() {
         MpgFile mpgFile = new MpgFile(getContext());
+        String fileName = mpgFile.getContext().getString(R.string.TestFile);
 
-        String headerLine = mpgFile.ReadHeaderLine();
+        String headerLine = mpgFile.ReadHeaderLine(fileName);
 
         assertEquals("GPS Time, Device Time, Longitude, Latitude,GPS Speed (Meters/second), Horizontal Dilution of Precision, Altitude, Bearing, G(x), G(y), G(z), G(calibrated),Miles Per Gallon(Instant)(mpg),Trip average MPG(mpg),Trip Distance(miles),Fuel used (trip)(gal),Trip Time(Since journey start)(s),Trip time(whilst stationary)(s)", headerLine);
     }
 
-    public void testReadLastLine() {
+    public void testReadFirstDataLine() {
         MpgFile mpgFile = new MpgFile(getContext());
+        String fileName = mpgFile.getContext().getString(R.string.TestFile);
 
-        String lastLine = mpgFile.ReadLastLine();
+        String firstLine = mpgFile.ReadFirstDataLine(fileName);
+
+        assertEquals("Fri May 22 07:00:26 EDT 2015,22-May-2015 07:00:25.730,-81.99970712,41.42961112,0.5,38.0,46.0,171.8,-9.25839233,-0.5112915,3.53204346,0.03712408,-,-,-,-,0,0", firstLine);
+    }
+
+    public void testReadLastDataLine() {
+        MpgFile mpgFile = new MpgFile(getContext());
+        String fileName = mpgFile.getContext().getString(R.string.TestFile);
+
+        String lastLine = mpgFile.ReadLastDataLine(fileName);
 
         assertEquals("Fri May 22 07:24:48 EDT 2015,22-May-2015 07:24:47.180,-82.15115221,41.4215944,0.0,6.0,178.0,0.0,-0.16299438,4.13757324,9.83526611,-0.00290112,255,33.63421249,9.9491272,0.29580379,1456,134.2539978", lastLine);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+
+
     }
 }
--- app/src/main/java/com/talz13/testmpgreader/MpgFile.java
+++ app/src/main/java/com/talz13/testmpgreader/MpgFile.java
@@ -2,26 +2,67 @@ package com.talz13.testmpgreader;
 
 import android.content.Context;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 /**
- * Created by b010396 on 5/26/15.
+ * Created by talz13 on 5/26/15.
  */
 public class MpgFile {
 
     private final Context context;
+    private File myFile;
+    private FileReader fileReader;
+//    private RandomAccessFile
 
     public MpgFile(Context context) {
         this.context = context;
     }
 
-    public boolean OpenFile() {
+    public boolean OpenFile(String filename) {
+        if (myFile == null)
+            myFile = new File(filename);
+        if (myFile.exists() && myFile.isFile() && myFile.canRead()) {
+            try {
+//                fileReader = new FileReader(myFile);
+//                fileReader
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }
         return false;
     }
 
-    public String ReadHeaderLine() {
+    public boolean CloseFile() {
+        if (myFile != null ) {
+            return true;
+        }
+        return false;
+    }
+
+    public String ReadHeaderLine(String filename) {
+        String headerLine = null;
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            headerLine = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return headerLine;
+    }
+
+    public String ReadLastDataLine(String filename) {
         return null;
     }
 
-    public String ReadLastLine() {
+    public String ReadFirstDataLine(String filename) {
         return null;
+    }
+
+    public Context getContext() {
+        return context;
     }
 }
